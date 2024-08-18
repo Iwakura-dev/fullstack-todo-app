1a65017e57eaff06e8260eca76a6817240433e82
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-19 01:30:50 +0300

    feat: 🎸 add auth and login method

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { TodoController } from './todo/todo.controller';
+import { TodoModule } from './todo/todo.module';
+
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:8080/nest')],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true,
+  }), MongooseModule.forRoot(process.env.DB_URI), AuthModule, TodoModule],
+  controllers: [AppController, TodoController],
   providers: [AppService],
 })
 export class AppModule { }
