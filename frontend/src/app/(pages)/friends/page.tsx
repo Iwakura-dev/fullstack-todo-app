6a9f8a96fb54163535d06eae62e99eeda96e637d
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-19 23:49:22 +0300

    feat: 🎸 add pages

---
+++ frontend/src/app/(pages)/friends/page.tsx
@@ -0,0 +1,11 @@
+import { Main } from "@/components/Main/Main";
+
+export default function Friends() {
+  return (
+    <div>
+      <Main title="Welcome to yout friends page!">
+        <h1>das</h1>
+      </Main>
+    </div>
+  );
+}
---
+++ frontend/src/app/(pages)/todos/page.tsx
@@ -0,0 +1,11 @@
+import { Main } from "@/components/Main/Main";
+
+export default function Todos() {
+  return (
+    <div>
+      <Main title="Welcome to your todos page!">
+        <h1>das</h1>
+      </Main>
+    </div>
+  );
+}
