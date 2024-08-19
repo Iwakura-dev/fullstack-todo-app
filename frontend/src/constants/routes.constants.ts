bbc723dead8595e348c1e31b29c984173ba2f443
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-19 23:47:37 +0300

    fix: 🐛 rechange name for first route

--- frontend/src/constants/routes.constants.ts
+++ frontend/src/constants/routes.constants.ts
@@ -2,8 +2,8 @@ import { v4 as uuidv4 } from 'uuid';
 export const routes = [
   {
     id: uuidv4(),
-    title: "Good Deeds",
-    href: '/good-deeds'
+    title: "Todos",
+    href: '/todos'
   },
   {
     id: uuidv4(),
