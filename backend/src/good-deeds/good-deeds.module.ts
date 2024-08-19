73aa686a1f74be98cbc9984d1a8db4d33032ef11
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-19 21:48:19 +0300

    feat: 🎸 connect good-deeds module to mongodv

--- backend/src/good-deeds/good-deeds.module.ts
+++ backend/src/good-deeds/good-deeds.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GoodDeedsController } from './good-deeds.controller';
 import { GoodDeedsService } from './good-deeds.service';
+import { MongooseModule } from "@nestjs/mongoose";
+import { GoodDeed, GoodDeedSchema } from "./schema/good-deeds.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: GoodDeed.name, schema: GoodDeedSchema }]),
+  ],
   controllers: [GoodDeedsController],
   providers: [GoodDeedsService]
 })
-export class GoodDeedsModule {}
+export class GoodDeedsModule { }
