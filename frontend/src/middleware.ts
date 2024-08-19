d32cac85a701d6f35a7c2f7d1fe66d7d11765fcb
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-20 01:12:02 +0300

    feat: 🎸 add middleware for the auth

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifyJwtToken } from "../utils/auth.utils";
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("jwt")?.value;
+  console.log("Token:", token);
+
+  const verifiedToken =
+    token &&
+    (await verifyJwtToken(token).catch((err) => {
+      console.log("Token verification error:", err);
+    }));
+
+  if (!verifiedToken) {
+    console.log("No valid token, checking routes...");
+
+    if (req.nextUrl.pathname.startsWith("/login") || req.nextUrl.pathname.startsWith("/register")) {
+      console.log("Allowing access to login or register route");
+      return NextResponse.next();
+    }
+
+    console.log("Redirecting to login page");
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  console.log("Valid token found, allowing access");
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
