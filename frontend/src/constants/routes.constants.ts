b96c5ca7a4053700416f5c95cd437b5c73e93aea
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-19 23:32:18 +0300

    fix: 🐛 name route and add new fields

--- frontend/src/constants/routes.constants.ts
+++ frontend/src/constants/routes.constants.ts
@@ -2,10 +2,12 @@ import { v4 as uuidv4 } from 'uuid';
 export const routes = [
   {
     id: uuidv4(),
-    title: "Home"
+    title: "Good Deeds",
+    href: '/good-deeds'
   },
   {
     id: uuidv4(),
-    title: "Friends"
+    title: "Friends",
+    href: '/friends'
   },
 ];
