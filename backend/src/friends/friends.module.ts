04857ba659c549aa2bbfa59a002f7ee44a493e9c
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-19 21:48:52 +0300

    feat: 🎸 connect friends module to mongodv

--- backend/src/friends/friends.module.ts
+++ backend/src/friends/friends.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FriendsService } from './friends.service';
 import { FriendsController } from './friends.controller';
+import { MongooseModule } from "@nestjs/mongoose";
+import { Friend, FriendSchema } from "./schema/friends.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Friend.name, schema: FriendSchema }]),
+  ],
   providers: [FriendsService],
   controllers: [FriendsController]
 })
-export class FriendsModule {}
+export class FriendsModule { }
