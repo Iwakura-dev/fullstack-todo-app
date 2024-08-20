7a92755ead6fd03b97a1a3bd1a5e55e177a4c3b9
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-20 22:51:32 +0300

    feat: 🎸 modifyed config to improvments a catch error

--- frontend/src/config/axios.config.ts
+++ frontend/src/config/axios.config.ts
@@ -8,12 +8,24 @@ const instance: AxiosInstance = axios.create({
     "Access-Control-Allow-Headers": "*",
   }
 });
+
 instance.interceptors.request.use(config => {
   const token = Cookies.get('jwt');
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
   return config;
+}, error => {
+  console.error("Request error:", error);
+  return Promise.reject(error);
 });
+
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    console.error("Axios error:", error);
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
